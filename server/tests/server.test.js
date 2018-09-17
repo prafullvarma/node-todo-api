a9a014a7e02835c490862bd5d80cf879f44e9484
Prafull Kumar Varma <prafull.varma.3@gmail.com>
2018-09-17 13:12:05 +0530

    Added test cases for /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,19 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'first dummy test'
 },{
+  _id: new ObjectID(),
   text: 'second dummy test'
 }];
+
+var someId = new ObjectID();
 
 beforeEach((done) => {
   Todo.deleteMany({}).then(() => {
@@ -72,4 +77,28 @@ describe('GET /todos', () => {
       })
       .end(done);
   });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+  it('should give a 404 for unmatching todo id', (done) => {
+    request(app)
+      .get(`/todos/someId.toHexString()`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return 404 for non ObjectID', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  })
 });
