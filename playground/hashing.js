741527e8433ba718a5e2cf779e57118adceddd7a
Prafull Kumar Varma <prafull.varma.3@gmail.com>
2018-09-21 12:42:41 +0530

    Hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 11
-}
+var password = "aaabbb1122";
 
-var token = jwt.sign(data, '111aaa');
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '111aaa');
+var hashedValue = "$2a$10$hw4wovHZN63RyXbSsFJpe./RYm0cfY217VlJxllGSC0.kazktCvzq";
+
+bcrypt.compare(password, hashedValue, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 11
+// }
+//
+// var token = jwt.sign(data, '111aaa');
+//
+// var decoded = jwt.verify(token, '111aaa');
 
 
 // var msg = "blah vlah dlah";
